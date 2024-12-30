b30121091091a6d0ee2d9a3a58256e75987667bf
Oguz Kazkayasi <oguz.kazkayasi@cleevio.com>
2024-12-30 09:44:37 +0300

    feat: tidy up formatting and indentation
    - Standardized indentation for better readability
    - Kept the same functionality in the setup of null-ls
    - No changes to dependencies or sources, just cleaned up code style

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,22 +1,22 @@
 return {
-    "nvimtools/none-ls.nvim",
-    dependencies = {
-        "nvimtools/none-ls-extras.nvim",
-    },
-    config = function()
-        local null_ls = require("null-ls")
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
+  config = function()
+    local null_ls = require("null-ls")
 
-        null_ls.setup({
-            sources = {
-                null_ls.builtins.formatting.stylua.with({
-                    extra_args = { "--config-path", vim.fn.expand("~/.config/nvim/stylua.toml") },
-                }),
-                null_ls.builtins.formatting.prettier,
-                require("none-ls.diagnostics.eslint_d"),
-                require("none-ls.code_actions.eslint_d"),
-            },
-        })
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua.with({
+          extra_args = { "--config-path", vim.fn.expand("~/.config/nvim/stylua.toml") },
+        }),
+        null_ls.builtins.formatting.prettier,
+        require("none-ls.diagnostics.eslint_d"),
+        require("none-ls.code_actions.eslint_d"),
+      },
+    })
 
-        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-    end,
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
 }
