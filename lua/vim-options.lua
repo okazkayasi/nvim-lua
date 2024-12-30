3c05f245e917ebf640e877f57a83cb6dbf5718b8
Oguz Kazkayasi <oguz.kazkayasi@cleevio.com>
2024-12-30 11:26:37 +0300

    feat: enhance navigation with smooth scrolling
    Added key mappings for smoother window navigation and search results. Now using `zz` to center the cursor after scrolling or searching, improving overall usability.

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -14,6 +14,13 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { noremap = true, silent = true })
+vim.keymap.set('n', 'n', 'nzz', { noremap = true, silent = true })
+vim.keymap.set('n', 'N', 'Nzz', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-o>', '<C-o>zz', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-i>', '<C-i>zz', { noremap = true, silent = true })
+
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
   group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
